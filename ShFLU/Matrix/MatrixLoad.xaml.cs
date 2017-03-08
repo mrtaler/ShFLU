a3912a9caf7745da46280dc2157c112201b3a2d0
Taler <mrtaler@me.com>
2017-03-08 16:18:28 +0300

    up

---
+++ ShFLU/Matrix/MatrixLoad.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShFLU.Matrix
+{
+    /// <summary>
+    /// Логика взаимодействия для MatrixLoad.xaml
+    /// </summary>
+    public partial class MatrixLoad : Window
+    {
+        public MatrixLoad()
+        {
+            DataContext = new MatrixLoadViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShFLU/Matrix/MatrixLoadViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using ShFLU.DataBase;
+using ShFLU.MVVM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShFLU.Matrix
+{
+  public  class MatrixLoadViewModel:ViewModelBase
+    {
+        private ShFluContext context = new ShFluContext();
+        private string MatrixFilePatch;
+        public ViewModelCommand LoadMatrixCommand { get; set; }
+        public ViewModelCommand SerialMatrixCommand { get; set; }
+        public MatrixLoadViewModel()
+        {
+            LoadMatrixCommand = new ViewModelCommand(LoadMatrix, true);
+            SerialMatrixCommand = new ViewModelCommand(SerialMatrix, false);
+        }
+        private void LoadMatrix(object param)
+        {
+            string str = "pkc7";
+            string filter = "GCS Skill files (*." + str + ")| *." + str + "| All Files(*.*) | *.* ";
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = @"D:\ДокументыЛинкевич\ПРИХОД ШФУ\ЕТК\02";
+            dlg.Filter = filter;
+            dlg.ShowDialog();
+            if (dlg.FileName != "")
+            {
+                MatrixFilePatch = dlg.FileName;
+                NotifyPropertyChanged("MatrixFilePatch");
+                SerialMatrixCommand.CanExecute = true;
+            }
+        }
+        private void SerialMatrix(object param)
+        {
+            string[] stringFromFile = File.ReadAllLines(MatrixFilePatch);
+            StringBuilder strResult = new StringBuilder();
+            foreach (var itemStringFromFile in stringFromFile)
+            {
+                strResult.Append(itemStringFromFile);
+                strResult.Append('\n');
+            }
+
+        }
+    }
+}
